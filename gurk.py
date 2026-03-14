89e81cffc7538831e8d0ab50ae314b052ec19bd8
Gurk <Vuthanhdang9392@yahoo.com.vn>
2026-03-14 10:34:36 -0400

    Update and rename guck.py to gurk.py

--- gurk.py
+++ gurk.py
@@ -3,14 +3,14 @@ from brain.thinker import think
 
 memory = load_memory()
 
-print("guck online")
+print("gurk online")
 
 while True:
     user = input("human: ")
 
     reply = think(user)
 
-    print("guck:", reply)
+    print("gurk:", reply)
 
     add_memory(memory, {"human": user, "guck": reply})
 ---
